4d6553c3f64ee5296af4275e313fbb348bb55953
lauranepo <lnepo@avereusa.com>
2024-03-23 23:15:49 -0700

    initialize and log in using spotify auth

--- src/App.js
+++ src/App.js
@@ -1,22 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 function App() {
+  // TODO: hide these?
+  const CLIENT_ID = process.env.REACT_APP_SECRET_CLIENT_ID;
+  const REDIRECT_URI = "http://localhost:3000";
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+  const RESPONSE_TYPE = "token";
+
+  const [token, setToken] = useState("");
+  const [playlists, setPlaylists] = useState([]);
+
+  const logout = () => {
+    setToken("");
+    window.localStorage.removeItem("token");
+  };
+
+  const getPlaylists = async () => {
+    const { data } = await axios.get("https://api.spotify.com/v1/me/playlists", {
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+    });
+
+    setPlaylists(data.items.map(p => p.name));
+  };
+
+  // TODO: use component to display each?
+  const showPlaylists = () => {
+    return playlists.map(playlist => (
+      <p key={playlist}>{playlist}</p>
+    ))
+  };
+
+  useEffect(() => {
+    const hash = window.location.hash;
+    let token = window.localStorage.getItem("token");
+
+    if (!token && hash) {
+      // TODO: fix this code I don't like it??
+      token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1];
+      window.location.hash = "";
+      window.localStorage.setItem("token", token);
+    }
+
+    setToken(token);
+  }, []);
+
+  // useEffect(() => {
+  //   getPlaylists();
+  // }, [token]);
+
+  // TODO: proper login/logout layout
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {!token ?
+          <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
+          : <button onClick={logout}>Logout</button>
+        }
+        {/* {showPlaylists()} */}
       </header>
     </div>
   );
