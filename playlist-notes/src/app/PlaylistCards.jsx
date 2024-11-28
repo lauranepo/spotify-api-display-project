3096fe7c698e19501380310e7d9e26018efe713b
lauranepo <laura.nepo@dmv.ca.gov>
2024-11-28 00:37:07 -0800

    formatting changes

--- playlist-notes/src/app/PlaylistCards.jsx
+++ playlist-notes/src/app/PlaylistCards.jsx
@@ -1,32 +1,43 @@
-import { Card, CardContent, CardMedia, Typography, Grid, CardActionArea } from "@mui/material";
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+  Grid,
+  CardActionArea,
+} from "@mui/material";
+import Link from "next/link";
 
 const PlaylistCards = ({ playlists }) => {
+  if (playlists === null || playlists.length === 0) {
+    return <Typography>no playlists</Typography>;
+  } else {
     return (
-        <Grid container spacing={2}>
-            {playlists.map((playlist, index) => (
-                <Grid item key={index} columnSpacing={{ xs: 4, sm: 6, md: 8 }}>
-                    <CardActionArea>
-                        <Card
-                            sx={{ height: 300, width: 200 }}
-                            variant="outlined"
-                        >
-                            <CardMedia
-                                component="image"
-                                sx={{ height: 200  }}
-                                image={playlist.images[0].url}
-                                title="playlist art"
-                            />
-                            <CardContent>
-                                <Typography>
-                                    {playlist.name}
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                    </CardActionArea>
-                </Grid>
-            ))}
-        </Grid>
-    )
-}
+      <Grid container spacing={2}>
+        {playlists.map((playlist, index) => {
+          if (playlist === null) return;
+          return (
+            <Grid item key={index} columnSpacing={{ xs: 4, sm: 6, md: 8 }}>
+              <CardActionArea>
+                <Link href={`/playlist/${playlist.id}`} />
+                <Card sx={{ height: 300, width: 200 }} variant="outlined">
+                  <CardMedia
+                    component="image"
+                    sx={{ height: 200 }}
+                    image={playlist?.images[0]?.url}
+                    title="playlist art"
+                  />
+                  <CardContent>
+                    <Typography>{playlist.name}</Typography>
+                  </CardContent>
+                </Card>
+              </CardActionArea>
+            </Grid>
+          );
+        })}
+      </Grid>
+    );
+  }
+};
 
 export default PlaylistCards;
--- playlist-notes/src/app/page.js
+++ playlist-notes/src/app/page.js
@@ -1,17 +1,19 @@
-"use client"
+"use client";
+"use strict";
 
 import styles from "./page.module.css";
 
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import { useEffect, useState } from "react";
+import axios from "axios";
 import { Button, Typography } from "@mui/material";
 import PlaylistCards from "./PlaylistCards";
 
 export default function Home() {
   const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID;
-  const REDIRECT_URI = "http://localhost:3000";
+  const REDIRECT_URI = "http://localhost:3000/";
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
   const RESPONSE_TYPE = "token";
+  const SCOPES = ["playlist-read-private", "playlist-read-collaborative"];
 
   const [token, setToken] = useState("");
   const [playlists, setPlaylists] = useState([]);
@@ -24,60 +26,89 @@ export default function Home() {
 
   const getPlaylists = async () => {
     try {
-      const { data } = await axios.get("https://api.spotify.com/v1/me/playlists", {
-        headers: {
-          Authorization: `Bearer ${token}`
+      const { data } = await axios.get(
+        "https://api.spotify.com/v1/me/playlists",
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
         },
-      });
-      setPlaylists(data.items);
+      );
+
+      if (data.items) {
+        setPlaylists(
+          data.items.filter((item) => {
+            if (item !== null) {
+              return item;
+            }
+          }),
+        ); // Set the playlists to state if data.items exists
+      } else {
+        console.error("No playlists found in response");
+      }
     } catch (error) {
       console.error(error);
     }
   };
 
   useEffect(() => {
+    console.log(playlists);
+  }, [playlists]);
+
+  useEffect(() => {
     const hash = window.location.hash;
     let token = window.localStorage.getItem("token");
 
     if (!token && hash) {
-      // TODO: fix this code I don't like it??
-      token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1];
-      window.location.hash = "";
-      window.localStorage.setItem("token", token);
+      const urlParams = new URLSearchParams(hash.substring(1));
+      token = urlParams.get("access_token");
+
+      if (token) {
+        window.localStorage.setItem("token", token);
+      } else {
+        console.error("No access token found in URL");
+      }
     }
 
     setToken(token);
   }, []);
 
   useEffect(() => {
-    getPlaylists();
+    if (token) {
+      getPlaylists();
+    }
   }, [token]);
 
-  // TODO: proper login/logout layout
   return (
     <div className={styles.main}>
       <header>
-        {!token ?
+        {!token ? (
           <Button
-              color="secondary"
-              variant="contained"
-              href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
-              classes={styles.rightAlign}>
-                Login to Spotify
+            color="secondary"
+            variant="contained"
+            href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPES}`}
+            classes={styles.rightAlign}
+          >
+            Login to Spotify
           </Button>
-            : 
-            <Button
-              color="secondary"
-              variant="contained"
-              onClick={onLogOut}
-              classes={styles.rightAlign}
-            >
-              Logout
-            </Button>
-        }
-        <Typography variant="h1">song description adder</Typography>
+        ) : (
+          <Button
+            color="secondary"
+            variant="contained"
+            onClick={onLogOut}
+            classes={styles.rightAlign}
+          >
+            Logout
+          </Button>
+        )}
+        <Typography variant="h2">pick a playlist to analyze</Typography>
       </header>
-      <PlaylistCards playlists={playlists}/>
+      {/* Conditional rendering of PlaylistCards */}
+      {playlists != null ? (
+        <PlaylistCards playlists={playlists} />
+      ) : (
+        <Typography>No playlists available.</Typography>
+      )}
     </div>
   );
 }
